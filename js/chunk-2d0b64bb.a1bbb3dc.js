654923abf360ee4c9207d627987a34e5eb858354
hamid <hamidbit99@gmail.com>
2021-08-29 10:56:06 +0430

    commit two
    panel

---
+++ js/chunk-2d0b64bb.a1bbb3dc.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0b64bb"],{"1d0b":function(e,n,t){"use strict";t.r(n);var i=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[e._v(" RepetitiveWords ")])},r=[],s={name:"RepetitiveWords"},u=s,c=t("2877"),l=Object(c["a"])(u,i,r,!1,null,null,null);n["default"]=l.exports}}]);
+//# sourceMappingURL=chunk-2d0b64bb.a1bbb3dc.js.map
